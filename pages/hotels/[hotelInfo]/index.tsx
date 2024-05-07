463ec1dc9e9718cc702530b2be854e2718f444df
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2024-05-07 17:31:16 +0530

    abc

--- pages/hotels/[hotelInfo]/index.tsx
+++ pages/hotels/[hotelInfo]/index.tsx
@@ -1,8 +1,26 @@
-import { getHotelDetails } from "@/pages/api/hotelInfo/hotelImg";
+import axios from "axios";
 import { GetServerSidePropsContext } from "next";
 
 export default function Index({ hotelDetails }: { hotelDetails: any }) {
-  return <div>hotel data : {JSON.stringify(hotelDetails)}</div>;
+  return (
+    <div>
+      hotel data : {JSON.stringify(hotelDetails.hotel_Image_Url, null, 2)}
+    </div>
+  );
+}
+
+async function getHotelDetails(hotelSlug: string) {
+  try {
+    const res = await axios.get(
+      `https://staybook-api-server-mu3dpypziq-el.a.run.app/hotels/fetch/fetchHotelInfo?hotel_Id=${encodeURIComponent(
+        hotelSlug
+      )}`
+    );
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching hotel details:", error);
+    throw error; // Rethrow the error to handle it in the calling function
+  }
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
