bf8cf0cc7c2fff0f4d6bee507caf4715a8f99654
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2024-05-07 16:30:40 +0530

    second

--- components/headers/CustomHotelHeader.tsx
+++ components/headers/CustomHotelHeader.tsx
@@ -16,6 +16,15 @@ export default function CustomHotelHeader(props: Props) {
       <meta name="twitter:description" content={props.metaDescription} />
       <meta name="twitter:url" content={props.canonicalUrl} />
       <meta name="twitter:image" content={props.metaImageUrl} />
+
+      {/* of tags */}
+      <meta property="og:card" content="summary_large_image" />
+      <meta property="og:title" content={props.hotelName} />
+      <meta property="og:description" content={props.metaDescription} />
+      <meta property="og:image" content={props.metaImageUrl} />
+      <meta property="og:url" content={props.canonicalUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Staybook" />
     </Head>
   );
 }
