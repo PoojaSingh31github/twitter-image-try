07cb24633295219f9ebc2ce790fe93f3a4ecae5a
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2024-05-07 18:29:40 +0530

    abcss

--- pages/hotels/[hotelInfo]/index.tsx
+++ pages/hotels/[hotelInfo]/index.tsx
@@ -1,19 +1,30 @@
+import CustomHotelHeader from "@/components/headers/CustomHotelHeader";
 import axios from "axios";
 import { GetServerSidePropsContext } from "next";
 import Image from "next/image";
 
 export default function Index({ hotelDetails }: { hotelDetails: any }) {
   return (
-    <div>
-      <Image
-        width={126}
-        height={126}
-        src={hotelDetails.hotel_Image_Url}
-        alt="img"
+    <>
+      <CustomHotelHeader
+        hotelName={"jai bala ji "}
+        metaDescription={"descrriptions"}
+        metaImageUrl={
+          "https://staybook.in/_next/image?url=https%3A%2F%2Fimages.staybook.in%2FStaybook-Hotel-Jai-Balaji-New-Delhi-Railway-Station%2FStaybookJaibalaji_reception_1.jpg&w=384&q=75"
+        }
+        canonicalUrl={"staybook.in"}
       />
-      <p>{hotelDetails.hotel_Name}</p>
-      <p>{hotelDetails.hotel_Description_List}</p>
-    </div>
+      <div>
+        <Image
+          width={126}
+          height={126}
+          src={hotelDetails.hotel_Image_Url}
+          alt="img"
+        />
+        <p>{hotelDetails.hotel_Name}</p>
+        <p>{hotelDetails.hotel_Description_List}</p>
+      </div>
+    </>
   );
 }
 
