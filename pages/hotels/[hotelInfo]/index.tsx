13c8ab038ae75c0c06039adb1b8f4c98edffc531
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2024-05-08 14:40:09 +0530

    meta tags

--- pages/hotels/[hotelInfo]/index.tsx
+++ pages/hotels/[hotelInfo]/index.tsx
@@ -1,7 +1,6 @@
 import CustomHotelHeader from "@/components/headers/CustomHotelHeader";
 import axios from "axios";
 import { GetServerSidePropsContext } from "next";
-import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
@@ -9,7 +8,7 @@ export default function Index({ hotelDetails }: { hotelDetails: any }) {
   const router = useRouter();
   return (
     <>
-      {/* <CustomHotelHeader
+      <CustomHotelHeader
         hotelName={
           hotelDetails.hotel_Name
             ? hotelDetails.hotel_Name
@@ -26,29 +25,7 @@ export default function Index({ hotelDetails }: { hotelDetails: any }) {
             : "https://images.staybook.in/Staybook-Hotel-Jai-Balaji-New-Delhi-Railway-Station/StaybookJaibalaji_reception_1.jpg"
         }
         canonicalUrl={`https://twitter-image-try.vercel.app//hotels/${router.query.hotelInfo}`}
-      /> */}
-      <Head>
-        <meta
-          property="og:title"
-          content="Philippine Standard Geographic Code API"
-        />
-        <meta property="og:site_name" content="Wareneutron - PSGC API" />
-        <meta
-          property="og:description"
-          content="API used for listing all the region, province, city, municipality, barangay, and its data. Philippine Standard Geographic Codes (PSGC) data came from Philippine Statistics Authority."
-        />
-        <meta property="og:url" content="https://psgc-api.wareneutron.com/" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="/assets/wareneutron_psgc_api_opengraph.jpg"
-        />
-        <meta property="og:image:alt" content="PSGC Banner" />
-        <meta property="og:image:width" content="1280" />
-        <meta property="og:image:height" content="640" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:image:alt" content="PSGC API Banner" />
-      </Head>
+      />
       <section>
         <div>
           <Image
